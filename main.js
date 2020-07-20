c024ae04b241b1319eb29cf246a533a873bb4859
Wanda <wmmcinturff@gmail.com>
2020-07-20 18:51:54 -0500

    tested datatypes

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log("Sh*t about to get real")
+//console.log("Sh*t about to get real")
+
+//let x;
+//console.log(x)
+
+//*Boolean
+//let x = true;
+//let y = false;
+
+//console.log(x,y)
+
+//if (x) {
+   // console.log("This is true")
+//} else {
+ //   console.log("This is false")
+//}
+
+
+//*Number
+
+// let number = 22;
+// console.log(number * 22)
+
+//* String
+
+//let string = 'Today is Monday!'
+
+//if (string === 'today is Monday') {
+   // console.log("YAYAA!!!")
+//}
+// console.log(typeof string)
+
+//let array = []
+
+let name = 'Dr.T',
+    age = "wouldn't you like to know...",
+    school = "NSS";
+
+
+let object = {
+     name,
+     age,
+     school
+}
+
+//let myFunction = () => {
+  //  return
+//}
+console.log(object.age)
